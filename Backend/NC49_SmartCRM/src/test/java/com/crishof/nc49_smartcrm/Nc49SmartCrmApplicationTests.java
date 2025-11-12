bf44b8e6e328673144e2b0fe7d45950bf880c12f
crishof <cristianhof@gmail.com>
2025-11-12 15:45:25 +0100

    create base project

---
+++ Backend/NC49_SmartCRM/src/test/java/com/crishof/nc49_smartcrm/Nc49SmartCrmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.crishof.nc49_smartcrm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Nc49SmartCrmApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
