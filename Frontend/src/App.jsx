4f83e914d93378cec1b6b6df64147bb3f746fcc7
adriel9-5 <adriel39102248@gmail.com>
2025-11-19 23:52:24 -0300

    fix: actualizaciones de estilos en sidebar

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,5 +1,7 @@
 
-import { Routes, Route, Router, BrowserRouter } from "react-router-dom";
+
+import { BrowserRouter, Routes, Route, Router } from "react-router-dom";
+import Sidebar from "./components/Sidebar/Sidebar";
 
 import Dashboard from "./pages/Dashboard";
 import Contactos from "./pages/Contactos";
@@ -8,29 +10,32 @@ import Correos from "./pages/Correos";
 import Segmentacion from "./pages/Segmentacion";
 import Recordatorios from "./pages/Recordatorios";
 import Analiticas from "./pages/Analiticas";
-import Sidebar from "./components/layouts/Sidebar";
 
 export default function App() {
   return (
-    <div>
+  
+      <div className="d-flex">
+        
+        {/* SIDEBAR */}
+        <Sidebar />
 
-    <Sidebar/>
+        {/* CONTENIDO A LA DERECHA */}
+        <div className="flex-grow-1 p-4">
+          <Routes>
+            <Route path="/" element={<Dashboard />} />
+            <Route path="/contactos" element={<Contactos />} />
+            <Route path="/whatsapp" element={<Whatsapp />} />
+            <Route path="/correos" element={<Correos />} />
+            <Route path="/segmentacion" element={<Segmentacion />} />
+            <Route path="/recordatorios" element={<Recordatorios />} />
+            <Route path="/analiticas" element={<Analiticas/>} />
+          </Routes>
+        </div>
 
-    <Routes>
-        <Route path="/" element={<Dashboard />} />
-        <Route path="/contactos" element={<Contactos />} />
-        <Route path="/whatsapp" element={<Whatsapp />} />
-        <Route path="/correos" element={<Correos />} />
-        <Route path="/segmentacion" element={<Segmentacion />} />
-        <Route path="/recordatorios" element={<Recordatorios />} />
-        <Route path="/analiticas" element={<Analiticas />} />
-    </Routes>
-    </div>
-    
+      </div>
    
-    
-       
   );
 }
+
 
 
