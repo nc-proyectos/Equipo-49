073dc790a98ad25e392bacdafa05c3039ae02b94
lautarofrioni <lauti.tatengue@hotmail.com>
2025-11-21 00:31:11 -0300

    avanzando con el dashboard

--- Frontend/src/pages/Dashboard.jsx
+++ Frontend/src/pages/Dashboard.jsx
@@ -2,8 +2,23 @@ import PeopleIcon from '@mui/icons-material/People';
 import NearMeIcon from '@mui/icons-material/NearMe';
 import ShowChartIcon from '@mui/icons-material/ShowChart';
 import WatchLaterIcon from '@mui/icons-material/WatchLater';
+import "./dashboard.css"
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import InputLabel from '@mui/material/InputLabel';
+import MenuItem from '@mui/material/MenuItem';
+import FormControl from '@mui/material/FormControl';
+import Select from '@mui/material/Select';
+import MoreHorizIcon from '@mui/icons-material/MoreHoriz';
 
 export default function Dashboard() {
+  
+  const [age, setAge] = React.useState('');
+
+  const handleChange = (event) => {
+    setAge(event.target.value);
+  };
+  
   return (
     <>
       <h2>Hola,Camila!</h2>
@@ -36,13 +51,38 @@ export default function Dashboard() {
           <p>Promedio mensual</p>
         </article>
         <article className='cuadro'>
-          <div className='porcentaje'>
+          <div className='porcentaje1'>
             <WatchLaterIcon/>
             <p>Pendientes</p>
           </div>
           <p>Seguimiento Hoy</p>
           <h2>23</h2>
           <p>0 urgentes, 0 normales</p>
+        </article>
+      </div>
+      <div className='dosCuadros'>
+        <article className='dosCuadros1'>
+          <h3>Titulo</h3>
+          <Box className="icono1" sx={{ minWidth: 120 }}>
+          <FormControl fullWidth>
+          <InputLabel id="demo-simple-select-label">Age</InputLabel>
+          <Select
+            labelId="demo-simple-select-label"
+            id="demo-simple-select"
+            value={age}
+            label="Age"
+            onChange={handleChange}
+          >
+            <MenuItem value={10}>Ten</MenuItem>
+            <MenuItem value={20}>Twenty</MenuItem>
+            <MenuItem value={30}>Thirty</MenuItem>
+          </Select>
+          </FormControl>
+        </Box>
+        </article>
+        <article className='dosCuadros2'>
+          <h2>Titulo</h2>
+          <MoreHorizIcon className='Icono2'/>
         </article>
       </div>
     </>
