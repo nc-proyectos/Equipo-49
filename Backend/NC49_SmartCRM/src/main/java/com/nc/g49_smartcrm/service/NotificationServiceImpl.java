2ec24e871e5b7a9aae5845b6e55c0cfa9f5ce5b1
Cristian Hoffmann <cristianhof@gmail.com>
2025-11-26 10:19:05 +0100

    Notificacióm recibida en el service

---
+++ Backend/NC49_SmartCRM/src/main/java/com/nc/g49_smartcrm/service/NotificationServiceImpl.java
@@ -0,0 +1,29 @@
+package com.nc.g49_smartcrm.service;
+
+
+import com.nc.g49_smartcrm.websocket.NotificationWebSocketHandler;
+import lombok.AllArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
+import org.springframework.web.socket.TextMessage;
+
+@Service
+@AllArgsConstructor
+public class NotificationServiceImpl implements NotificationService {
+    private static final Logger log = LoggerFactory.getLogger(NotificationServiceImpl.class);
+
+    private final NotificationWebSocketHandler wsHandler;
+
+    @Override
+    public void sendReminder(Long userId, Long taskId, String message) {
+        String payload = String.format("{\"type\":\"TASK_REMINDER\",\"taskId\":%d,\"message\":\"%s\"}", taskId, message.replace("\"", "\\\""));
+        try {
+            wsHandler.sendToUser(userId, new TextMessage(payload));
+            log.info("[Notification service] Reminder sent to user {}: {}", userId, payload);
+        } catch (Exception e) {
+            log.error("Error sending ws to user {}", userId, e);
+        }
+    }
+}
+
