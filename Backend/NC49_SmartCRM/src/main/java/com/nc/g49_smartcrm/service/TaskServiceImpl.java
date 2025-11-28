d47083d8b87c7d57902aacf003f0b9aef18092f2
Cristian Hoffmann <cristianhof@gmail.com>
2025-11-28 16:14:50 +0100

    set currentUser to task

--- Backend/NC49_SmartCRM/src/main/java/com/nc/g49_smartcrm/service/TaskServiceImpl.java
+++ Backend/NC49_SmartCRM/src/main/java/com/nc/g49_smartcrm/service/TaskServiceImpl.java
@@ -5,6 +5,7 @@ import com.nc.g49_smartcrm.dto.TaskResponse;
 import com.nc.g49_smartcrm.exception.TaskNotFoundException;
 import com.nc.g49_smartcrm.mapper.TaskMapper;
 import com.nc.g49_smartcrm.model.Task;
+import com.nc.g49_smartcrm.model.User;
 import com.nc.g49_smartcrm.repository.TaskRepository;
 import jakarta.transaction.Transactional;
 import lombok.AllArgsConstructor;
@@ -20,6 +21,7 @@ public class TaskServiceImpl implements TaskService {
     private final TaskRepository taskRepository;
     private final TaskMapper taskMapper;
     private final QueueClient queueClient;
+    private UserService userService;
 
     @Transactional
     @Override
@@ -27,7 +29,9 @@ public class TaskServiceImpl implements TaskService {
 
         Task task = taskMapper.toEntity(taskRequest);
         task.setCreatedAt(Instant.now());
-        task.setUserId(taskRequest.getUserId());
+
+        User user = userService.getCurrentUser();
+        task.setUserId(user.getId());
         task.setStatus(Task.Status.PENDING);
         Task saved = taskRepository.save(task);
 
