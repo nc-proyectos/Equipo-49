25f1b325abc80093970fbbc7b9603b05b69a77e1
loginLayer <84354397+loginLayer@users.noreply.github.com>
2025-12-05 00:48:05 -0300

    refactor: Eliminados archivos/carpetas subidos por error

--- src/pages/Analiticas.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import Sidebar from "../components/layouts/SidebarDashboard";
-
-const Analiticas = () => {
-  return <h2>Analíticas</h2>;
-};
-
-export default Analiticas;
